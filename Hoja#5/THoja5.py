6d0049aae143891732bcadf899e52d087d30d261
SebastianUVG <156974025+SebastianUVG@users.noreply.github.com>
2024-03-13 18:33:47 -0600

    Terminando funcinoes

--- Hoja#5/THoja5.py
+++ Hoja#5/THoja5.py
@@ -32,4 +32,41 @@ class Proceso:
 
         yield self.env.timeout(0)  # Para que el proceso no termine inmediatamente y permita ejecutar la simulación
 
-   
+    def proceso(self, nom, cantRam, NIns, tiempo_proceso):
+        yield self.env.timeout(tiempo_proceso)
+        print(f'{nom}. Solicita {cantRam} de RAM (New)')
+        tiempo_inicial = self.env.now
+
+        yield self.ram.get(cantRam)
+        print(f'{nom}. Solicitud aceptada por {cantRam} de RAM (Admited)')
+
+        tiempo_final = 0
+
+        while tiempo_final < NIns:
+            with self.cpu.request() as req:
+                yield req
+                if (NIns - tiempo_final) >= self.velocidad:
+                    efec = self.velocidad
+                else:
+                    efec = (NIns - tiempo_final)
+
+                print(f'{nom}. CPU ejecutara {efec} instrucciones. (Ready)')
+                yield self.env.timeout(efec / self.velocidad)
+
+                tiempo_final += efec
+                print(f'{nom}. CPU ({tiempo_final}/{NIns}) completado. (Running)')
+
+            decision = random.randint(1, 2)
+
+            if decision == 1 and tiempo_final < NIns:
+                with self.wait.request() as req2:
+                    yield req2
+                    yield self.env.timeout(1)
+                    print(f'{nom}. Realizadas operaciones de entrada/salida. (Waiting)')
+
+        yield self.ram.put(cantRam)
+        print(f'{nom}. Retorna {cantRam} de RAM. (Terminated)')
+        self.Ttotal += (self.env.now - tiempo_inicial)
+        self.tiempo.append(self.env.now - tiempo_inicial)
+
+# Definición de variables
